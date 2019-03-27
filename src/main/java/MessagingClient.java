4f8fc19dfbbc76813dafdfba574c696384ba6b02
Soufyan Bargach <soufyanb2@hotmail.com>
2019-03-27 14:40:23 +0100

    Implement feedback

--- src/main/java/MessagingClient.java
+++ src/main/java/MessagingClient.java
@@ -1,81 +1,78 @@
-import com.google.gson.Gson;
-import models.Message;
-import models.Request;
+package com.cmtelecom.text.sdk;
 
-import models.Response;
-import utils.HttpHelper;
+import com.google.gson.Gson;
+import com.cmtelecom.text.sdk.models.Message;
+import com.cmtelecom.text.sdk.models.Request;
+import com.cmtelecom.text.sdk.models.Response;
+import com.cmtelecom.text.sdk.utils.HttpHelper;
 
 public class MessagingClient {
-
+    
     private String productToken;
-
-
-    public MessagingClient(String productToken) {
+    
+    
+    public MessagingClient( String productToken ) {
         this.productToken = productToken;
     }
-
-
-    public Response.HttpResponseBody sendTextMessage(String messageText, String from, String[] to) {
-
-        try {
-            MessageBuilder builder = new MessageBuilder(messageText, from, to);
-
-            Message message = builder.Build();
-
-            String body = GetHttpPostBody(productToken, message);
-
-            String result = HttpHelper.post(Config.ApiUrl, body);
-
-            return getResponseBody(result);
-
-        } catch (Exception e) {
-            System.out.println("Please check your request body.");
-            throw e;
-        }
+    
+    
+    public Response.HttpResponseBody sendTextMessage( String messageText, String from, String[] to ) {
+        
+        MessageBuilder builder = new MessageBuilder( messageText, from, to );
+        
+        Message message = builder.Build();
+        
+        String body = GetHttpPostBody( productToken, message );
+        
+        String result = HttpHelper.post( Config.ApiUrl, body );
+        
+        return getResponseBody( result );
     }
-
-
-    public Response.HttpResponseBody sendMessage(Message message) {
-
-        try {
-            String body = GetHttpPostBody(productToken, message);
-
-            String result = HttpHelper.post(Config.ApiUrl, body);
-
-            return getResponseBody(result);
-        } catch (Exception e) {
-            System.out.println("Please check your request body.");
-            throw e;
-        }
+    
+    
+    public Response.HttpResponseBody sendMessage( Message message ) {
+        String body = GetHttpPostBody( productToken, message );
+        String result = HttpHelper.post( Config.ApiUrl, body );
+        return getResponseBody( result );
     }
-
-
+    
+    
     /// <summary>
     ///     Gets the HTTP post body.
     /// </summary>
     /// <param name="apiKey">The API key.</param>
     /// <param name="message">The message to send.</param>
     /// <returns></returns>
-    protected static Response.HttpResponseBody getResponseBody(String body) {
-        Response.HttpResponseBody result = new Gson().fromJson(body, Response.HttpResponseBody.class);
-        return result;
+    protected static Response.HttpResponseBody getResponseBody( String body ) {
+        try {
+            Response.HttpResponseBody result = new Gson().fromJson( body, Response.HttpResponseBody.class );
+            return result;
+        }
+        catch ( Exception e ) {
+            throw new RuntimeException( "Invalid json response-body", e );
+        }
     }
-
-
+    
+    
     /// <summary>
     ///     Gets the HTTP post body.
     /// </summary>
     /// <param name="apiKey">The API key.</param>
     /// <param name="message">The message to send.</param>
     /// <returns></returns>
-    protected static String GetHttpPostBody(String productToken, Message message) {
+    protected static String GetHttpPostBody( String productToken, Message message ) {
         Request.Messages messages = new Request.Messages();
         Request.MessagesEnvelope request = new Request.MessagesEnvelope();
-        request.setAuthentication(new Request.Authentication(productToken));
-        Message[] msg = new Message[]{message};
-        request.setMessages(msg);
-        messages.setMessages(request);
-        return new Gson().toJson(messages);
+        request.setAuthentication( new Request.Authentication( productToken ) );
+        Message[] msg = new Message[]{ message };
+        request.setMessages( msg );
+        messages.setMessages( request );
+        try {
+            return new Gson().toJson( messages );
+        }
+        catch ( Exception e ) {
+            throw new RuntimeException( "Invalid request body", e );
+        }
     }
-
+    
 }
--- src/main/java/utils/HttpHelper.java
+++ src/main/java/utils/HttpHelper.java
@@ -1,55 +1,56 @@
-package utils;
+package com.cmtelecom.text.sdk.utils;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
-import java.net.ConnectException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
 
 public class HttpHelper {
-    private static String sendRequest(String method, String url, String data) {
+    private static String sendRequest( String method, String url, String data ) {
+        HttpURLConnection conn = null;
         try {
 
-            URL obj = new URL(url);
-            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            URL obj = new URL( url );
+            conn = (HttpURLConnection) obj.openConnection();
 
             //add request header
-            con.setRequestMethod(method);
+            conn.setRequestMethod( method );
 
-            con.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty( "Content-Type", "application/json" );
 
 
-            if (data != null) {
+            if ( data != null ) {
                 // Send request
-                con.setDoOutput(true);
-                try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
-                    wr.writeBytes(data);
+                conn.setDoOutput( true );
+                try ( DataOutputStream wr = new DataOutputStream( conn.getOutputStream() ) ) {
+                    wr.writeBytes( data );
                 }
             }
 
             StringBuilder response = new StringBuilder();
-            try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+            try ( BufferedReader in = new BufferedReader( new InputStreamReader( conn.getInputStream() ) ) ) {
                 String inputLine;
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
+                while ( (inputLine = in.readLine()) != null ) {
+                    response.append( inputLine );
                 }
             }
 
             return response.toString();
-        } catch (ConnectException ce) {
-            System.out.println(ce.getMessage());
-            return ce.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return e.toString();
+        }
+        catch ( Exception e ) {
+            throw new RuntimeException( String.format( "Error sending request to url: %s with data: %s", url, data ), e );
+        }
+        finally {
+            if ( conn != null ) {
+                conn.disconnect();
+            }
         }
     }
 
     // HTTP POST request
-    public static String post(String url, String urlParameters) {
-        return sendRequest("POST", url, urlParameters);
+    public static String post( String url, String urlParameters ) {
+        return sendRequest( "POST", url, urlParameters );
     }
 
 }
