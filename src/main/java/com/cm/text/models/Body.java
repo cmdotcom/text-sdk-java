6e460d6da45b5adf01959be17f3ccccba6e17c1e
Bas Gijzen <bas.gijzen@cm.com>
2024-01-08 17:11:48 +0100

    Start of implementation

--- src/main/java/com/cm/text/models/Body.java
+++ src/main/java/com/cm/text/models/Body.java
@@ -4,19 +4,19 @@ import com.google.gson.annotations.SerializedName;
 
 public class Body {
 
-    /// <summary>
-    ///     The actual text body of the message.
-    ///     By default the CM gateway interprets messages as if sent with the standard 7 bit GSM encoding.
-    ///     If you want to send messages using e.g. Arabic, Cyrillic of Greek characters
-    ///     you will need to use the unicode UCS2 encoding.
-    ///     Set the <see cref="Type" /> to Auto to let the gateway do the encoding detection.
-    ///     Please note that there are a few limitations to using unicode encoded messages:
-    ///     Unicode messages can contain up to 70 characters. In the case of multipart messages, this becomes 66 characters per
-    ///     part.
-    ///     You will need to POST the XML or JSON file. A HTTP GET request cannot handle the Unicode characters
-    ///     Another note is that not all operators in the world are able to handle Unicode messages, so you will need to test
-    ///     for which operators it works.
-    /// </summary>
+    /***
+     * The actual text body of the message.
+     * By default the CM gateway interprets messages as if sent with the standard 7 bit GSM encoding.
+     * If you want to send messages using e.g. Arabic, Cyrillic of Greek characters
+     * you will need to use the unicode UCS2 encoding.
+     * Set the <see cref="Type" /> to Auto to let the gateway do the encoding detection.
+     * Please note that there are a few limitations to using unicode encoded messages:
+     * Unicode messages can contain up to 70 characters. In the case of multipart messages, this becomes 66 characters per
+     * part.
+     * You will need to POST the XML or JSON file. A HTTP GET request cannot handle the Unicode characters
+     * Another note is that not all operators in the world are able to handle Unicode messages, so you will need to test
+     * for which operators it works.
+     */
     @SerializedName("content")
     public String Content;
 
